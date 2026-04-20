2a5b093ba3c9c2ee2be4c3f24e610e8d0f77ef0f
thedevaman <devaman2508@gmail.com>
2026-04-20 08:06:20 +0530

    form populate for edit

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { Button, Card, Form, Input, message } from 'antd'
 import { useForm } from 'antd/es/form/Form'
 import { Save } from 'lucide-react'
-import React from 'react'
+import React, { useState } from 'react'
 import { useFile } from './zustand/useFile'
 import moment from 'moment'
 import { nanoid } from 'nanoid'
@@ -9,7 +9,8 @@ import 'animate.css';
 
 function App() {
   const [form] = useForm()
-  const {files, setFile} = useFile()
+  const {files, setFile, updateFile} = useFile()
+  const [editable,setEditable] = useState(null)
 
    const createFile = (value)=>{
 
@@ -23,12 +24,24 @@ function App() {
 
   }
 
+  const readFile =(file)=>{
+    setEditable(file)
+    form.setFieldsValue(file)
+  }
+
+  const updateChanges = (value) =>{
+     value.date = moment().format("DD MMM YYYY, hh:mm A")
+
+     updateFile(editable.id,value)
+    console.log(editable.id)
+  }
+
   return (
     <div className='bg-sky-100 min-h-screen'>
       <div className='w-80  fixed top-0 left-0 h-full overflow-auto flex flex-col gap-4 p-4 bg-sky-500 animate__animated animate__slideInLeft'>
         {
           files.map((file,index)=>(
-            <Card hoverable>
+            <Card key={index} hoverable onClick={()=>readFile(file)}>
            <Card.Meta
            className='capitalize'
            title={file.filename}
@@ -41,7 +54,7 @@ function App() {
       <div className='px-12 py-8 ml-80'>
         <Card className='shadow-lg animate__animated animate__fadeInDown'>
           <h1 className='text-4xl font-bold'>Text Editor</h1>
-          <Form form={form} className='!mt-6' layout='vertical' onFinish={createFile}>
+          <Form form={form} className='!mt-6' layout='vertical' onFinish={editable ? updateChanges : createFile}>
             <Form.Item
             label={<h1 className='font-medium'>Filename</h1>} 
             rules={[{required:true}]}
@@ -66,7 +79,11 @@ function App() {
             </Form.Item>
 
              <Form.Item>
-              <Button htmlType='submit' type='primary' danger='true'>Save <Save /> </Button>
+              <Button htmlType='submit' type='primary' danger= {editable} >
+                
+               {editable?'Update':'Save'}
+                
+                <Save /> </Button>
             </Form.Item>
           </Form>
         </Card>
